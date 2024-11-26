bed2addfb5020bee863c03421e140f7e1e01849e
Afonso Neves <up202108884@up.pt>
2024-11-26 10:40:23 +0000

    Database Update

---
+++ database/data.sql
@@ -0,0 +1,30 @@
+-----------------------------------------
+-- DATA INSERTION
+-----------------------------------------
+
+INSERT INTO users(first_name, last_name, username, email, password, profile_picture, preferred_address, is_admin)
+VALUES ('Sir', 'Admin', 'adminUno', 'admin@example.com', '1234', 'images/default.png', row('Rua Mouzinho da Silveira, nº 13', 'Porto', 'Porto', '4050-430', 'Portugal', '910000000'),TRUE),
+       ('Not', 'Admin', 'sadUser1', 'user@example.com', '1234', 'images/default.png', NULL,FALSE),
+       ('André','Pinto', 'apinto', 'up202108856@fe.up.pt','1234','images/default.png', NULL, TRUE);
+
+INSERT INTO products(title,description, images, type, platform, region, price, rating, visibility)
+VALUES ('Fifa 25 Steam Europe', 'The Ultimate EA Football game experience', '', 'Game','Steam', 'Europe',49.99, 4.2, TRUE),
+       ('Terraria Steam Europe', 'A cozy game to play by the fireplace', '', 'Game','Steam', 'Europe',14.99, 4.2, TRUE),
+       ('PICO PARK Steam Europe', 'PICO PARK is a cooperaive local/online multiplayer action puzzle', '', 'Game', 'Steam', 'Europe', 4.99, 5, TRUE),
+       ('Cities Skylines II Steam Europe', 'Create your own megapolis in the new Paradox sandbox game', '', 'Game', 'Steam', 'Europe', 69.99, 3.2, TRUE);
+
+INSERT INTO orders(price, status, order_date, deliver_date, billing_address, user_id)
+VALUES (49.99, 'Completed','2024-03-24 00:00:00', '2024-03-25 00:00:00', NULL,1),
+       (14.99, 'Completed','2024-04-24 00:00:00', '2024-04-27 00:00:00', NULL,2),
+       (74.98,'Completed','2024-10-27 00:00:00', '2024-10-28 00:00:00',NULL, 3);
+
+INSERT INTO transactions(date,amount,provider, status,order_id)
+VALUES ('2024-03-24 00:01:00', 49.99, 'PayPal', 'Completed', 1),
+       ('2024-10-27 00:01:00', 74.98, 'PayPal', 'Completed',3);
+
+INSERT INTO product_keys(key, order_id, product_id)
+VALUES ('jhafuau81bak18', 1, 1),
+       ('jjs18s8f71', 2, 2),
+       ('kafmahn1',3, 4),
+       ('jjak1iav1001',3, 3),
+       ('afabfia131',NULL, 3);
--- database/schema.sql
+++ database/schema.sql
@@ -46,7 +46,6 @@ CREATE TYPE address AS (
     phone_number VARCHAR(255)
 );
 
-
 CREATE TYPE provider AS ENUM ('Card', 'PayPal', 'MBWay', 'Revolut');
 
 CREATE TYPE product_type AS ENUM ('Game', 'DLC', 'Game Points', 'Subscription');
@@ -70,6 +69,7 @@ CREATE TABLE users (
     password VARCHAR(255) NOT NULL,
     profile_picture TEXT NOT NULL DEFAULT 'default.jpg',
     preferred_address address,
+    remember_token VARCHAR(255),
     is_admin BOOLEAN NOT NULL DEFAULT FALSE,
     CONSTRAINT auth_username_length CHECK (LENGTH(username) >= 5 AND LENGTH(username) <= 20)
 );
@@ -94,7 +94,6 @@ CREATE TABLE orders (
     CONSTRAINT order_delivery_date_ck CHECK (deliver_date > order_date),
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE SET DEFAULT
 );
-
 
 CREATE TABLE transactions (
     id SERIAL PRIMARY KEY,
@@ -124,7 +123,6 @@ CREATE TABLE discounts (
     FOREIGN KEY (promotion_id) REFERENCES promotions(id) ON DELETE CASCADE
 );
 
-
 CREATE TABLE products (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL CONSTRAINT product_title_uk UNIQUE,
@@ -150,7 +148,6 @@ CREATE TABLE product_keys (
     FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE,
     FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
 );
-
 
 CREATE TABLE categories (
     id SERIAL PRIMARY KEY,
@@ -322,31 +319,3 @@ CREATE TRIGGER trigger_ensure_cart_stock
     ON shopping_cart
     FOR EACH ROW
 EXECUTE FUNCTION ensure_cart_stock();
-
-INSERT INTO users(first_name, last_name, username, email, password, profile_picture, preferred_address, is_admin)
-VALUES ('Sir', 'Admin', 'adminUno', 'admin@example.com', '1234', 'images/default.png', row('Rua Mouzinho da Silveira, nº 13', 'Porto', 'Porto', '4050-430', 'Portugal', '910000000'),TRUE),
-       ('Not', 'Admin', 'sadUser1', 'user@example.com', '1234', 'images/default.png', NULL,FALSE),
-       ('André','Pinto', 'apinto', 'up202108856@fe.up.pt','1234','images/default.png', NULL, TRUE);
-
-INSERT INTO products(title,description, images, type, platform, region, price, rating, visibility)
-VALUES ('Fifa 25 Steam Europe', 'The Ultimate EA Football game experience', '', 'Game','Steam', 'Europe',49.99, 4.2, TRUE),
-       ('Terraria Steam Europe', 'A cozy game to play by the fireplace', '', 'Game','Steam', 'Europe',14.99, 4.2, TRUE),
-       ('PICO PARK Steam Europe', 'PICO PARK is a cooperaive local/online multiplayer action puzzle', '', 'Game', 'Steam', 'Europe', 4.99, 5, TRUE),
-       ('Cities Skylines II Steam Europe', 'Create your own megapolis in the new Paradox sandbox game', '', 'Game', 'Steam', 'Europe', 69.99, 3.2, TRUE);
-
-INSERT INTO orders(price, status, order_date, deliver_date, billing_address, user_id)
-VALUES (49.99, 'Completed','2024-03-24 00:00:00', '2024-03-25 00:00:00', NULL,1),
-       (14.99, 'Completed','2024-04-24 00:00:00', '2024-04-27 00:00:00', NULL,2),
-       (74.98,'Completed','2024-10-27 00:00:00', '2024-10-28 00:00:00',NULL, 3);
-
-INSERT INTO transactions(date,amount,provider, status,order_id)
-VALUES ('2024-03-24 00:01:00', 49.99, 'PayPal', 'Completed', 1),
-       ('2024-10-27 00:01:00', 74.98, 'PayPal', 'Completed',3);
-
-
-INSERT INTO product_keys(key, order_id, product_id)
-VALUES ('jhafuau81bak18', 1, 1),
-       ('jjs18s8f71', 2, 2),
-       ('kafmahn1',3, 4),
-       ('jjak1iav1001',3, 3),
-       ('afabfia131',NULL, 3);
