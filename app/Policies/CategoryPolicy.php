9679fa334d3b6efdd011c908c70b15282dbaeaed
Afonso Neves <up202108884@up.pt>
2024-11-27 04:47:46 +0000

    Create CategoryPolicy.php

---
+++ app/Policies/CategoryPolicy.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+
+class CategoryPolicy
+{
+    /**
+     * Shows the first 5 categories using pagination.
+     */
+    public function index(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Shows the category of a given user for a given product.
+     */
+    public function list(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Show the category page for a given id.
+     */
+    public function show(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Shows the create category widget.
+     */
+    public function create(User $user): bool
+    {
+        return $user->is_admin;
+    }
+
+    /**
+     * Shows the edit category widget.
+     */
+    public function edit(User $user ): bool
+    {
+        return $user->is_admin;
+    }
+
+    /**
+     * Inserts a new category.
+     */
+    public function store(User $user): bool
+    {
+        return $user->is_admin;
+    }
+
+    /**
+     * Updates a category.
+     */
+    public function update(User $user): bool
+    {
+        return $user->is_admin;
+    }
+
+    /**
+     * Deletes a category.
+     */
+    public function destroy(User $user): bool
+    {
+        return $user->is_admin;
+    }
+}
