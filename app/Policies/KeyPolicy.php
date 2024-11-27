1bb40438bfcd7b4f640c22a08641da8ecf0ac2ce
Afonso Neves <up202108884@up.pt>
2024-11-27 05:30:11 +0000

    Keys Review

---
+++ app/Policies/KeyPolicy.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\ProductKey as Key;
+
+class KeyPolicy
+{
+    /**
+     * Shows the key with the given id.
+     */
+    public function fetch(User $user, Key $key): bool
+    {
+        $keys = $user->productKeys();
+
+        for ($i = 0; $i < $keys->count(); $i++) {
+            if ($keys[$i] == $key) {
+                return true;
+            }
+        }
+
+        return $user->is_admin;
+    }
+
+    /**
+     * Shows the keys of a given user.
+     */
+    public function listUserKeys(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Shows the create key widget.
+     */
+    public function create(User $user): bool
+    {
+        return $user->is_admin;
+    }
+
+    /**
+     * Shows the edit key widget.
+     */
+    public function edit(User $user ): bool
+    {
+        return $user->is_admin;
+    }
+
+    /**
+     * Inserts a new key.
+     */
+    public function store(User $user): bool
+    {
+        return $user->is_admin;
+    }
+
+    /**
+     * Updates a key.
+     */
+    public function update(User $user): bool
+    {
+        return $user->is_admin;
+    }
+
+    /**
+     * Deletes a key.
+     */
+    public function destroy(User $user): bool
+    {
+        return $user->is_admin;
+    }
+}
