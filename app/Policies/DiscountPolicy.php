46d8b75e0607dba9adc84b38d31baefa5e0be3b3
Afonso Neves <up202108884@up.pt>
2024-11-27 04:51:24 +0000

    Create DiscountPolicy.php

---
+++ app/Policies/DiscountPolicy.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+
+class DiscountPolicy
+{
+    /**
+     * Fetches a discount.
+     */
+    public function fetch(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Lists all discounts.
+     */
+    public function list(User $user): bool
+    {
+        return $user->is_admin;
+    }
+
+    /**
+     * Shows the create discount widget.
+     */
+    public function create(User $user): bool
+    {
+        return $user->is_admin;
+    }
+
+    /**
+     * Shows the edit discount widget.
+     */
+    public function edit(User $user ): bool
+    {
+        return $user->is_admin;
+    }
+
+    /**
+     * Inserts a new discount.
+     */
+    public function store(User $user): bool
+    {
+        return $user->is_admin;
+    }
+
+    /**
+     * Updates a discount.
+     */
+    public function update(User $user): bool
+    {
+        return $user->is_admin;
+    }
+
+    /**
+     * Deletes a discount.
+     */
+    public function destroy(User $user): bool
+    {
+        return $user->is_admin;
+    }
+}
